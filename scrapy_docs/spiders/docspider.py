89ab693417a13ee617dd2c2fbe5b6ba653fce902
B-rade <bpramberg@gmail.com>
2019-10-17 21:47:22 -0500

    changed to parse using robots.txt and added tqdm progress bars

--- scrapy_docs/spiders/docspider.py
+++ scrapy_docs/spiders/docspider.py
@@ -1,88 +1,57 @@
 # -*- coding: utf-8 -*-
-import json
-import urllib.parse as urlparse
-
 import scrapy
+from tqdm import tqdm
 
 
 class DocRootCrawler(scrapy.Spider):
     name = 'docs-root'
     allowed_domains = ['docs.microsoft.com']
-    start_urls = ['https://docs.microsoft.com/en-us/']
+    start_urls = ['https://docs.microsoft.com/_sitemaps/sitemapindex.xml']
 
     def parse(self, response):
-        NEXT_PAGE_SELECTOR = '.directory-cols li>a::attr(href)'
-        next_page = response.css(NEXT_PAGE_SELECTOR).getall()
-        for page in next_page:
-            yield scrapy.Request(
-                response.urljoin(page),
-                callback=self.parse_toc
-            )
+        NEXT_PAGE_SELECTOR = '//loc/text()'
+        response.selector.remove_namespaces()
+        index_urls = response.xpath(NEXT_PAGE_SELECTOR).getall()
+        for index_url in tqdm(index_urls):
+            if self.should_parse(index_url):
+                yield scrapy.Request(
+                    index_url,
+                    callback=self.parse_index
+                )
 
-    def parse_toc(self, response):
-        if response.url.endswith('index'):
-            url = response.url.rstrip('index')
-        else:
-            url = response.url
-        url = urlparse.urljoin(url, 'toc.json')
-        yield scrapy.Request(
-            url,
-            callback=self._parse_toc,
+    def parse_index(self, response):
+        NEXT_PAGE_SELECTOR = '//loc/text()'
+        response.selector.remove_namespaces()
+        urls = response.xpath(NEXT_PAGE_SELECTOR).getall()
+        for url in tqdm(urls):
+            if self.should_parse(url):
+                yield {'url': url}
+
+    def should_parse(self, url):
+        return all([
+            'en-us' in url,  # only parse english docs
+            '/api/' not in url,  # don't parse api docs
+            ]
         )
 
-    def _parse_toc(self, response):
-        if response.status == 404:
-            self.logger.warning(f'Bad URL resolved. Could not find TOC. {response.status} {response.url}')
-        yield {
-            'status': response.status,
-            'url': response.url
-        }
 
-
-class TOCCrawler(scrapy.Spider):
-    name = 'docs-toc'
+class DocCrawler(scrapy.Spider):
+    name = 'docs-docs'
     allowed_domains = ['docs.microsoft.com']
 
     def start_requests(self):
-        with open('results/docs-root.json', 'r') as file:
-            responses = json.load(file)
-        good_urls = self._response_gen(responses)
-        return [
+        urls = self._url_gen()
+        urls = (x['url'] for x in urls)
+        return (
             scrapy.Request(
                 url,
-                callback=self.parse_toc
-            ) for url in good_urls
-        ]
+                callback=self.parse_doc
+            ) for url in urls
+        )
 
-    def _response_gen(self, responses):
-        for response in responses:
-            if response['status'] == 200:
-                yield response['url']
-
-    def parse_toc(self, response):
-        try:
-            json_response = json.loads(response.body_as_unicode())
-        except json.decoder.JSONDecodeError:
-            self.logger.warning(f'Invalid JSON: {response.url}')
-            return
-        for item in json_response['items']:
-            yield from self.resolve_toc(response, item)
-
-    def resolve_toc(self, response, item):
-        url = response.url.rstrip('toc.json')
-        if item.get('tocHref'):
-            yield scrapy.Request(
-                urlparse.urljoin(url, item['tocHref']),
-                callback=self.parse_toc
-            )
-        if item.get('href'):
-            yield scrapy.Request(
-                urlparse.urljoin(url, item['href']),
-                callback=self.parse_doc,
-            )
-        if item.get('children'):
-            for child in item['children']:
-                yield from self.resolve_toc(response, child)
+    def _url_gen(self):
+        with open('results/docs-root.json', 'r') as file:
+            yield from file
 
     def parse_doc(self, response):
         IMG_SELECTOR = 'img[data-linktype=relative-path]::attr(src)'
--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    'Click>=7.0',
+    'tqdm',
+    'scrapy',
+]
 
 setup_requirements = ['pytest-runner', ]
 
